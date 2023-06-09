88e42ddc252cbc2613435f8ae9b601c5378b6665
Tim te Beek <timtebeek@gmail.com>
2023-06-09 23:21:00 +0200

    Initial structure (#1)
    * Initial structure
    
    * Remove out folder
    
    * Gradle wrapper 7.6.1

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("org.openrewrite.build.recipe-library") version "latest.release"
+}
+
+group = "org.openrewrite.recipe"
+description = "Hibernate ORM Migration"
+
+val rewriteVersion = rewriteRecipe.rewriteVersion.get()
+dependencies {
+    implementation(platform("org.openrewrite:rewrite-bom:$rewriteVersion"))
+    implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:$rewriteVersion"))
+    implementation("org.openrewrite:rewrite-java")
+    implementation("org.openrewrite.recipe:rewrite-migrate-java")
+    implementation("org.openrewrite.recipe:rewrite-java-dependencies")
+
+    testImplementation("org.openrewrite:rewrite-java-17")
+    testImplementation("org.openrewrite:rewrite-test")
+    testImplementation("org.openrewrite:rewrite-gradle")
+    testImplementation("org.openrewrite:rewrite-maven")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:latest.release")
+}
