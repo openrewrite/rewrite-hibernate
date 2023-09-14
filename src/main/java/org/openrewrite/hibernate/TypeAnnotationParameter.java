acbf1e9e91dfe8a68c8f4bf8f516c1369d38eab3
AlekSimpson <alek@asu.me>
2023-09-13 18:25:35 -0700

    first draft

--- src/main/java/org/openrewrite/hibernate/TypeAnnotationParameter.java
+++ src/main/java/org/openrewrite/hibernate/TypeAnnotationParameter.java
@@ -15,35 +15,32 @@
  */
 package org.openrewrite.hibernate;
 
-import org.jetbrains.annotations.NotNull;
-import org.openrewrite.ExecutionContext;
-import org.openrewrite.Recipe;
-import org.openrewrite.Tree;
-import org.openrewrite.TreeVisitor;
+import org.openrewrite.*;
 import org.openrewrite.internal.ListUtils;
 import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.AnnotationMatcher;
+import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
+import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.*;
 import org.openrewrite.marker.Markers;
 
 import java.time.Duration;
 import java.util.Collections;
 
+@SuppressWarnings({"NullableProblems", "DataFlowIssue"})
 public class TypeAnnotationParameter extends Recipe {
-
-    //private static final String FQN_TYPE_ANNOTATION = "org.hibernate.annotations.Type";
-    private static final AnnotationMatcher FQN_TYPE_ANNOTATION = new AnnotationMatcher("@org.hibernate.annotations Type(..)");
-    private static final AnnotationMatcher TYPE_DEF_MATCHER = new AnnotationMatcher("@org.hibernate.annotations TypeDef(..)");
-    private static final AnnotationMatcher TYPE_DEFS_MATCHER = new AnnotationMatcher("@org.hibernate.annotations TypeDefs(..)");
+    private static final AnnotationMatcher FQN_TYPE_ANNOTATION = new AnnotationMatcher("@org.hibernate.annotations.Type");
+    private static final AnnotationMatcher TYPE_DEF_MATCHER = new AnnotationMatcher("@org.hibernate.annotations.TypeDef");
+    private static final AnnotationMatcher TYPE_DEFS_MATCHER = new AnnotationMatcher("@org.hibernate.annotations.TypeDefs");
 
     @Override
-    public @NotNull String getDisplayName() {
+    public String getDisplayName() {
         return "@Type annotation type parameter migration";
     }
 
     @Override
-    public @NotNull String getDescription() {
+    public String getDescription() {
         return "Hibernate 6.x has 'type' parameter of type String replaced with 'value' of type class.";
     }
 
@@ -51,91 +48,92 @@ public class TypeAnnotationParameter extends Recipe {
     public @Nullable Duration getEstimatedEffortPerOccurrence() {
         return Duration.ofMinutes(1);
     }
-    //FQN_TYPE_ANNOTATION.equals(type.getFullyQualifiedName())
 
     @Override
-    public @NotNull TreeVisitor<?, ExecutionContext> getVisitor() {
-        return new JavaVisitor<ExecutionContext>() {
-            @Override
-            public @NotNull J visitAnnotation(J.Annotation annotation, @NotNull ExecutionContext executionContext) {
-                J.Annotation a = (J.Annotation)super.visitAnnotation(annotation, executionContext);
-                JavaType.FullyQualified type = TypeUtils.asFullyQualified(a.getType());
-                if (type != null) {
-                    if (FQN_TYPE_ANNOTATION.matches(a)) {
-                        System.out.println("found @Type");
-                        return visitTypeAnnotation(a, executionContext);
-                    }else if (TYPE_DEF_MATCHER.matches(a) || TYPE_DEFS_MATCHER.matches(a)) {
-                        System.out.println("found @TypeDef or @TypeDefs");
-                        return visitTypeDefAnnotation(a, executionContext);
-                    }
+    public TreeVisitor<?, ExecutionContext> getVisitor() {
+        return new TypeAnnotationParameterVisitor();
+    }
+
+    private static class TypeAnnotationParameterVisitor extends JavaVisitor<ExecutionContext> {
+        @Override
+        public J visitAnnotation(J.Annotation annotation, ExecutionContext executionContext) {
+            System.out.println("getting here");
+            J.Annotation a = (J.Annotation)super.visitAnnotation(annotation, executionContext);
+            JavaType.FullyQualified type = TypeUtils.asFullyQualified(a.getType());
+            if (type != null) {
+                if (FQN_TYPE_ANNOTATION.matches(a)) {
+                    return visitTypeAnnotation(a);
+                }else if (TYPE_DEF_MATCHER.matches(a) || TYPE_DEFS_MATCHER.matches(a)) {
+                    // TODO: figure out way to check ahead of time if code contains @Type. If it does, this code shouldn't run
+                    maybeRemoveImport("org.hibernate.annotations.TypeDef");
+                    maybeRemoveImport("org.hibernate.annotations.TypeDefs");
+                    maybeRemoveImport("com.vladmihalcea.hibernate.type.json.JsonStringType");
+                    return null;
                 }
-                return a;
             }
 
-            private J visitTypeDefAnnotation(J.Annotation a, ExecutionContext ctx) {
-                maybeRemoveImport("org.hibernate.annotations.TypeDef");
-                maybeRemoveImport("org.hibernate.annotations.TypeDefs");
-                return null;
-            }
-
-            private J visitTypeAnnotation(J.Annotation annotation, ExecutionContext ctx) {
-                final boolean isOnlyParameter = annotation.getArguments().size() == 1;
-                J.Annotation a = annotation;
-                a = a.withArguments(ListUtils.map(a.getArguments(), arg -> {
-                    // check if arg is an assignment
-                    if (!(arg instanceof J.Assignment)) {
-                        return arg;
-                    }
-                    // check if arg meets assignment conditions
-                    J.Assignment assignment = (J.Assignment) arg;
-                    if (checkArgumentAssignmentConditions(assignment)) {
-                        return annotation;
-                    }
-
-                    J.Identifier paramName = (J.Identifier) assignment.getVariable();
-                    String fqTypeName = (String) ((J.Literal) assignment.getAssignment()).getValue(); // json
-                    boolean fqTypeNameIsJson = fqTypeName.equals("json");
-                    fqTypeName = fqTypeNameIsJson ? "JsonType" : fqTypeName;
-                    String simpleTypeName = getSimpleName(fqTypeName);
-                    JavaType typeOfNewValue = JavaType.buildType(fqTypeName);
-
-                    J.FieldAccess fa = buildFieldAccess(isOnlyParameter, assignment, simpleTypeName, typeOfNewValue);
-
-                    maybeAddImport(fqTypeName);
-                    if (isOnlyParameter) {
-                        return fa;
-                    }
-                    return assignment.withVariable(paramName.withSimpleName("value")).withAssignment(fa);
-                }));
-
-                return a;
-            }
-
-            private J.FieldAccess buildFieldAccess(boolean isOnlyParameter, J.Assignment assignment, String simpleTypeName, JavaType typeOfNewValue) {
-                return new J.FieldAccess(
-                        Tree.randomId(),
-                        isOnlyParameter ? Space.EMPTY : assignment.getAssignment().getPrefix(),
-                        assignment.getAssignment().getMarkers(),
-                        new J.Identifier(Tree.randomId(), Space.EMPTY, Markers.EMPTY, Collections.emptyList(), simpleTypeName, typeOfNewValue, null),
-                        JLeftPadded.build(new J.Identifier(Tree.randomId(), Space.EMPTY, Markers.EMPTY, Collections.emptyList(), "class", null, null)),
-                        JavaType.buildType("java.lang.Class")
-                );
-            }
-
-            private boolean checkArgumentAssignmentConditions(J.Assignment assignment) {
-                return !(assignment.getVariable() instanceof J.Identifier
-                         && "type".equals(((J.Identifier) assignment.getVariable()).getSimpleName())
-                         && assignment.getAssignment() instanceof J.Literal);
-            }
-        };
-    }
-
-    private static String getSimpleName(String fqName) {
-        int idx = fqName.lastIndexOf('.');
-        if (idx > 0 && idx < fqName.length() - 1) {
-            return fqName.substring(idx + 1);
+            return a;
         }
-        return fqName;
-    }
 
+        private J visitTypeAnnotation(J.Annotation a) {
+            final boolean isOnlyParameter = a.getArguments().size() == 1;
+            J.Annotation ann = a.withArguments(ListUtils.map(a.getArguments(), arg -> {
+                // check if arg is an assignment
+                if (!(arg instanceof J.Assignment)) {
+                    return arg;
+                }
+                // check if arg meets assignment conditions
+                J.Assignment assignment = (J.Assignment) arg;
+                if (checkArgumentAssignmentConditions(assignment)) {
+                    return arg;
+                }
+
+                J.Identifier paramName = (J.Identifier) assignment.getVariable();
+                String fqTypeName = (String) ((J.Literal) assignment.getAssignment()).getValue(); // json
+                boolean fqTypeNameIsJson = fqTypeName.equals("json");
+                fqTypeName = fqTypeNameIsJson ? "JsonType" : fqTypeName;
+                String simpleTypeName = getSimpleName(fqTypeName);
+                JavaType typeOfNewValue = JavaType.buildType(fqTypeName);
+
+                J.FieldAccess fa = buildFieldAccess(isOnlyParameter, assignment, simpleTypeName, typeOfNewValue);
+
+                maybeAddImport("io.hypersistence.utils.hibernate.type.json.JsonType");
+                // NOTE: seems like we want to keep the 'value = ' before arguments even when its the only argument? Not sure why this conditional is here
+                //if (isOnlyParameter) {
+                //    return fa;
+                //}
+
+                return assignment
+                        .withVariable(paramName.withSimpleName("value"))
+                        .withAssignment(fa);
+            }));
+
+            return ann;
+        }
+
+        private J.FieldAccess buildFieldAccess(boolean isOnlyParameter, J.Assignment assignment, String simpleTypeName, JavaType typeOfNewValue) {
+            return new J.FieldAccess(
+                    Tree.randomId(),
+                    isOnlyParameter ? assignment.getAssignment().getPrefix() : Space.EMPTY,
+                    assignment.getAssignment().getMarkers(),
+                    new J.Identifier(Tree.randomId(), Space.EMPTY, Markers.EMPTY, Collections.emptyList(), simpleTypeName, typeOfNewValue, null),
+                    JLeftPadded.build(new J.Identifier(Tree.randomId(), Space.EMPTY, Markers.EMPTY, Collections.emptyList(), "class", null, null)),
+                    JavaType.buildType("java.lang.Class")
+            );
+        }
+
+        private boolean checkArgumentAssignmentConditions(J.Assignment assignment) {
+            return !(assignment.getVariable() instanceof J.Identifier
+                     && "type".equals(((J.Identifier) assignment.getVariable()).getSimpleName())
+                     && assignment.getAssignment() instanceof J.Literal);
+        }
+
+        private static String getSimpleName(String fqName) {
+            int idx = fqName.lastIndexOf('.');
+            if (idx > 0 && idx < fqName.length() - 1) {
+                return fqName.substring(idx + 1);
+            }
+            return fqName;
+        }
+    }
 }
