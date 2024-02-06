c9c13989b419789f09561fd5465b3b1ca92fa68b
Tim te Beek <timtebeek@gmail.com>
2024-02-06 22:10:18 +0000

    refactor: OpenRewrite best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.recipes.OpenRewriteBestPractices?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/hibernate/TypeAnnotationParameterTest.java
+++ src/test/java/org/openrewrite/hibernate/TypeAnnotationParameterTest.java
@@ -16,6 +16,7 @@
 package org.openrewrite.hibernate;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
@@ -28,6 +29,7 @@ class TypeAnnotationParameterTest implements RewriteTest {
         spec.recipe(new TypeAnnotationParameter()).parser(JavaParser.fromJavaVersion().classpath("hibernate-core"));
     }
 
+    @DocumentExample
     @Test
     void onlyOneParameter() {
         rewriteRun(
