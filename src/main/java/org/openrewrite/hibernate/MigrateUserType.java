53a09337122cb7df9d02cbdc1119e420945870e2
Laurens Westerlaken <laurens.westerlaken@jdriven.com>
2024-10-04 10:37:59 +0200

    imports

--- src/main/java/org/openrewrite/hibernate/MigrateUserType.java
+++ src/main/java/org/openrewrite/hibernate/MigrateUserType.java
@@ -25,15 +25,7 @@ import org.openrewrite.java.JavaTemplate;
 import org.openrewrite.java.JavaVisitor;
 import org.openrewrite.java.MethodMatcher;
 import org.openrewrite.java.search.UsesType;
-import org.openrewrite.java.tree.Expression;
-import org.openrewrite.java.tree.J;
-import org.openrewrite.java.tree.ControlParentheses;
-import org.openrewrite.java.tree.JRightPadded;
-import org.openrewrite.java.tree.JavaType;
-import org.openrewrite.java.tree.Space;
-import org.openrewrite.java.tree.Statement;
-import org.openrewrite.java.tree.TypeTree;
-import org.openrewrite.java.tree.TypeUtils;
+import org.openrewrite.java.tree.*;
 import org.openrewrite.marker.Markers;
 
 import javax.annotation.Nullable;
