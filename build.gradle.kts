63fc09b9c5d4fda78376a8442d4d1a31941c00b9
Tim te Beek <tim@moderne.io>
2025-04-07 23:57:21 +0200

    Adopt classes from `@TypeDef` (#56)
    * Adopt classes from `@TypeDef`
    
    * Store & retrieve type def aliases on class declaration

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ description = "Hibernate ORM Migration"
 
 recipeDependencies {
     parserClasspath("jakarta.persistence:jakarta.persistence-api:latest.release")
+    parserClasspath("org.hibernate:hibernate-core:5.6.15.Final")
     parserClasspath("org.hibernate.orm:hibernate-core:6.5.1.Final")
 }
 
