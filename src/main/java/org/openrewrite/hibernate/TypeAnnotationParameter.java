99f7361ff568b795625de8cbc25bb4023b2b3844
Tim te Beek <tim@moderne.io>
2026-01-06 23:07:00 +0100

    Revert "The annotation arguments are never parameterized classes"
    This reverts commit 267ed8903346c5ccfb5343e293577605512ef089.

--- src/main/java/org/openrewrite/hibernate/TypeAnnotationParameter.java
+++ src/main/java/org/openrewrite/hibernate/TypeAnnotationParameter.java
@@ -264,10 +264,21 @@ public class TypeAnnotationParameter extends Recipe {
 
     private static @Nullable String getFullyQualifiedTypeName(Expression expr) {
         if (expr instanceof J.FieldAccess) {
-            String fqName = ((J.FieldAccess) expr).toString();
-            return fqName.endsWith(".class") ? fqName.substring(0, fqName.length() - 6) : fqName;
+            JavaType.Parameterized parameterizedType = TypeUtils.asParameterized((expr).getType());
+            if (parameterizedType != null) {
+                JavaType typeArgument = parameterizedType.getTypeParameters().get(0);
+                JavaType.FullyQualified fqType = TypeUtils.asFullyQualified(typeArgument);
+                if (fqType != null) {
+                    return fqType.getFullyQualifiedName();
+                }
+            }
+            return stripClassSuffix(((J.FieldAccess) expr).toString());
         }
         return null;
+    }
+
+    private static String stripClassSuffix(String fqName) {
+        return fqName.endsWith(".class") ? fqName.substring(0, fqName.length() - 6) : fqName;
     }
 
     private static String getSimpleName(String fqName) {
